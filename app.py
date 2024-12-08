033800eb78a1c02088b643701af3896474dd545c
ruter <arturkummer08@gmail.com>
2024-12-08 09:51:26 +0100

    made something

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import random
 
 app = Flask(__name__)
 
+with open('questions.json') as f:
+    questions = json.load(f)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def index():
+    if not questions:
+        return "No more questions!"
+    question = random.choice(list(questions.keys()))
+    return render_template('index.html', question=question)
 
+@app.route('/answer', methods=['POST'])
+def answer():
+    question = request.form['question']
+    answer = request.form['answer']
+    print(questions)
+    if question in questions and questions[question] == answer:
+        del questions[question]
+    if not questions:
+        return "No more questions!"
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
