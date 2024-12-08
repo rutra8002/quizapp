fa486ede46b920791c089aefa15de55a82d7ad47
ruter <arturkummer08@gmail.com>
2024-12-08 13:55:04 +0100

    make it delete not straight from db

--- app.py
+++ app.py
@@ -17,30 +17,38 @@ with app.app_context():
 
 correct_answers = 0
 wrong_answers = 0
+questions_list = []
+questions_loaded = False
+
+@app.before_request
+def load_questions():
+    global questions_list, questions_loaded
+    if not questions_loaded:
+        questions_list = Question.query.all()
+        questions_loaded = True
 
 @app.route('/')
 def index():
-    total_questions = Question.query.count()
+    total_questions = len(questions_list)
     if total_questions == 0:
         return redirect(url_for('quiz_completed'))
-    question = random.choice(Question.query.all())
+    question = random.choice(questions_list)
     return render_template('index.html', question=question.question, correct_answers=correct_answers, total_questions=total_questions)
 
 @app.route('/answer', methods=['POST'])
 def answer():
-    global correct_answers, wrong_answers
+    global correct_answers, wrong_answers, questions_list
     question_text = request.form['question']
     answer_text = request.form['answer']
-    question = Question.query.filter_by(question=question_text).first()
+    question = next((q for q in questions_list if q.question == question_text), None)
     if question:
         if question.answer == answer_text:
             correct_answers += 1
         else:
             wrong_answers += 1
-        db.session.delete(question)
-        db.session.commit()
+        questions_list.remove(question)
 
-    if Question.query.count() == 0:
+    if len(questions_list) == 0:
         return redirect(url_for('quiz_completed'))
     return redirect(url_for('index'))
 
